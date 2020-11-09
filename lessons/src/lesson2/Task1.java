01c3b6fd53535ce87bdaa7d6974d7868fecb23aa
Sobik <53480103+tango-del@users.noreply.github.com>
2020-11-09 22:48:17 +0200

    init

--- lessons/src/lesson2/Task1.java
+++ lessons/src/lesson2/Task1.java
@@ -1,7 +1,22 @@
 package lesson2;
 
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
+        findAverageValue();
+    }
 
+    public static void findAverageValue(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter first value:");
+        short firstValue = scanner.nextShort();
+        System.out.println("Enter second value:");
+        short secondValue = scanner.nextShort();
+        double averageValue = (double) (firstValue + secondValue) / 2;
+        System.out.println(averageValue);
     }
 }
+/*
+Программа, которая находит среднее арифметическое значение двух чисел.
+ */
