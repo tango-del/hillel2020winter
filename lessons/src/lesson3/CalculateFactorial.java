4957660b19dd6a8e70066d9f4c05010947633c0e
Sobik <53480103+tango-del@users.noreply.github.com>
2020-11-12 23:15:22 +0200

    update CalculateFactorial with while

--- lessons/src/lesson3/CalculateFactorial.java
+++ lessons/src/lesson3/CalculateFactorial.java
@@ -2,6 +2,7 @@ package lesson3;
 
 /*
 Дано число n при помощи цикла for посчитать факториал n!
+Перепишите программы с использованием цикла while.
  */
 
 import java.util.Scanner;
@@ -11,14 +12,31 @@ public class CalculateFactorial {
         init();
     }
 
-    public static void init(){
+    public static void init() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Input number to calculate factorial:");
         long number = scanner.nextInt();
+        calculateWithFor(number);
+        calculateWithWhile(number);
+    }
+
+    public static void calculateWithFor(long number) {
         long factorial = 1;
         for (int i = 1; i <= number; i++) {
             factorial *= i;
         }
-        System.out.println(factorial);
+        System.out.printf("Calculated with for : %s", factorial);
+        System.out.println();
+
+    }
+
+    public static void calculateWithWhile(long number) {
+        long factorial = 1;
+        int count = 1;
+        while (count <= number) {
+            factorial *= count;
+            count++;
+        }
+        System.out.printf("Calculated with while : %s", factorial);
     }
 }
