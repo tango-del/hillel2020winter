a59924a87172774de04c756834cd6fc8f3adbdd9
Sobik <53480103+tango-del@users.noreply.github.com>
2020-11-12 23:03:47 +0200

    update OutputOddNumbers with while

--- lessons/src/lesson3/OutputOddNumbers.java
+++ lessons/src/lesson3/OutputOddNumbers.java
@@ -2,18 +2,30 @@ package lesson3;
 
 /*
 При помощи цикла for вывести на экран нечетные числа от 1 до 99
+Перепишите программы с использованием цикла while.
  */
 
 public class OutputOddNumbers {
     public static void main(String[] args) {
-        init();
+        //outputOddWithFor();
+        outputOddWithWhile();
     }
 
-    public static void init() {
+    public static void outputOddWithFor() {
         for (int i = 0; i <= 100; i++) {
             if (i % 2 != 0) {
                 System.out.println(i);
             }
+        }
+    }
+
+    public static void outputOddWithWhile(){
+        int number = 0;
+        while (number <= 100) {
+            if (number % 2 != 0) {
+                System.out.println(number);
+            }
+            number++;
         }
     }
 }
