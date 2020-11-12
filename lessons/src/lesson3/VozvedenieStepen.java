0f05c8a769d96f0109445f84674957268137af63
Sobik <53480103+tango-del@users.noreply.github.com>
2020-11-12 23:46:21 +0200

    VozvedenieStepen

---
+++ lessons/src/lesson3/VozvedenieStepen.java
@@ -0,0 +1,27 @@
+package lesson3;
+
+/*
+Даны переменные x и n вычислить x^n
+ */
+
+import java.util.Scanner;
+
+public class VozvedenieStepen {
+    public static void main(String[] args) {
+        init();
+    }
+
+    public static void init(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number");
+        int number = scanner.nextInt();
+        System.out.println("Input pow");
+        int pow = scanner.nextInt();
+
+        int totalNumber = 1;
+        for (int i = 1; i <= pow; i++) {
+            totalNumber *= number;
+        }
+        System.out.printf("%s in pow %s is : %s", number, pow, totalNumber);
+    }
+}
