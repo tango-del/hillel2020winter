f64bd5346675d4dff21f5361bcaec2a0071e9cc1
Sobik <53480103+tango-del@users.noreply.github.com>
2020-11-13 09:11:28 +0200

    MultiplicationTable

---
+++ lessons/src/lesson3/MultiplicationTable.java
@@ -0,0 +1,25 @@
+package lesson3;
+
+/*
+Необходимо вывести на экран таблицу умножения на Х: (любое число вводимое из консоли)
+ */
+
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        init();
+    }
+
+    public static void init(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input number for multiplication table");
+        int number = scanner.nextInt();
+        int[] arrayMultiTable = new int[20];
+        for (int i = 0; i < arrayMultiTable.length; i++) {
+            int tempNumber = number * (i + 1);
+            System.out.println(tempNumber);
+        }
+
+    }
+}
