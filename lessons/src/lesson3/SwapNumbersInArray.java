b1a88c3aa4883b0cc2f1ebd5518e4dd07a911994
Sobik <53480103+tango-del@users.noreply.github.com>
2020-11-13 13:14:38 +0200

    add average arbitrary

--- lessons/src/lesson3/SwapNumbersInArray.java
+++ lessons/src/lesson3/SwapNumbersInArray.java
@@ -1,7 +1,7 @@
 package lesson3;
 /*
 11.Поменять наибольший и наименьший элементы массива местами
-
+12.Найти среднее арифметическое всех элементов массива
  */
 
 public class SwapNumbersInArray {
@@ -17,6 +17,8 @@ public class SwapNumbersInArray {
         findMinMaxNumbers(array);
 
         printArray(array);
+
+        findAverageArbitrary(array);
     }
 
     public static void findMinMaxNumbers(int[] arr) {
@@ -36,13 +38,25 @@ public class SwapNumbersInArray {
         }
         System.out.println("min: " + minNumber + " index: " + indexMin);
         System.out.println("max: " + maxNumber + " index: " + indexMax);
+
         swapMinMaxNumbers(arr, indexMin,indexMax);
     }
 
+    //swap indexes of max and min numbers in array
     public static void swapMinMaxNumbers(int[] arr, int indexMin, int indexMax) {
         int temp = arr[indexMin];
         arr[indexMin] = arr[indexMax];
         arr[indexMax] = temp;
+    }
+
+    //find sum of all numbers in array and calc average arbitrary
+    public static void findAverageArbitrary(int[] arr){
+        int sumNumber = 0;
+        for (int i : arr) {
+            sumNumber += i;
+        }
+        float averageNumber = (float) sumNumber / arr.length;
+        System.out.printf("Average arbitrary of array: %.2f", averageNumber);
     }
 
     //output array
