a2e28c9dfc8e18299c0e388f5178967503562b5d
tango <53480103+tango-del@users.noreply.github.com>
2020-11-27 15:28:39 +0200

    lesson7 working remove methods

--- lessons/src/lesson7/Main.java
+++ lessons/src/lesson7/Main.java
@@ -3,80 +3,115 @@ package lesson7;
 TODO Реализовать простую коллекцию для String на базе массива.
  добавить следующие методы для работы с коллекцией:
  добавлять (add), (по индеку или значению)
+ Добавление без индекса - добавление в конец
+ добавление по индексу -все элементы что находятся в права от индекса копируем в право
  удалять(delete), (по индексу или значению)
  доставать по индексу (get)
 */
 
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args) {
-        String str = "Sobik";
+        String string = "Sobik";
 
-        //создаю массив char с длиной строки str увеличенно на 1
-        char[] test = new char[str.length() + 1];
-        //копирую символы строки str в массив char
-        copySymbols(str, test);
-        //вывод
-        System.out.println("---copied test---");
-        System.out.println(Arrays.toString(test));
+//        System.out.println("---addSymbolToString---");
+//        String updateString = addSymbolToString(string, 'l', 2);
+//        System.out.println(updateString);
+//
+//        System.out.println("---extendString(string)---");
+//        String school = extendString(updateString, " School");
+//        System.out.println(school);
+//
+//        System.out.println("---extendString(char[])---");
+//        String ex1 = " School";
+//        String school2 = extendString(updateString, ex1.toCharArray());
+//        System.out.println(school2);
+//
+//        System.out.println("---removeSymbolFromString(index)---");
+//        String ex2 = removeSymbolFromString(updateString, 2);
+//        System.out.println(ex2);
 
-        addSymbolToString(test, 'l',  2);
-
-        System.out.println("---add symbol test---");
-        System.out.println(Arrays.toString(test));
-
-        removeSymbolFromString(test, 2);
-
-
+        System.out.println("---removeSymbolFromString(symbol)---");
+        String ex3 = removeSymbolFromString(string, 'b');
+        System.out.println(ex3);
     }
 
-    public static void removeSymbolFromString(char[] arrChar, int removeSymbolAtIndex) {
-        //создание массива char с длиной другого массива - 1
-        char[] newArr = new char[arrChar.length - 1];
+    public static String removeSymbolFromString(String string, char removeChar) {
+        //создание массива char с длиной string - 1
+        char[] newArr = new char[string.length() - 1];
+
+        copySymbolsToSmallerArray(string, newArr);
+
+        return new String(newArr);
+    }
+
+    public static String removeSymbolFromString(String string, int removeSymbolAtIndex) {
+        //создание массива char с длиной string - 1
+        char[] newArr = new char[string.length() - 1];
         /*
         один проход по массиву циклом в котором
-        до индекса removeSymbolAtIndex идёт копирование элементов у другого массива с таким же номером индекса
-        когда номер итерации >= removeSymbolAtIndex копирует значения второго массива начиная с индекса + 1
+        до индекса removeSymbolAtIndex идёт копирование элементов у строки с таким же номером индекса
+        когда номер итерации >= removeSymbolAtIndex копирует значения строки начиная с индекса + 1
          */
         for (int i = 0; i < newArr.length; i++) {
             if (i >= removeSymbolAtIndex) {
-                newArr[i] = arrChar[i + 1];
+                newArr[i] = string.charAt(i + 1);
             } else {
-                newArr[i] = arrChar[i];
+                newArr[i] = string.charAt(i);
             }
         }
-        System.out.println("---rem symbol newArr---");
-        System.out.println(Arrays.toString(newArr));
+        return new String(newArr);
+
         /*
         Два цикла по массиву, первый копирует все символы в новый массив до допустимой длинны
         второй цикл начинает итерацию с индекса removeSymbolAtIndex
-        копирует значения второго массива начиная с индекса + 1
+        копирует значения строки начиная с индекса + 1
         for (int i = 0; i < newArr.length; i++) {
-            newArr[i] = arrChar[i];
+            newArr[i] = string.charAt(i);
         }
         for (int i = removeSymbolAtIndex; i < newArr.length; i++) {
-            newArr[i] = arrChar[i + 1];
+            newArr[i] = string.charAt(i + 1);
         }
          */
 
     }
 
-    public static void addSymbolToString(char[] arrChar, char symbol, int moveSymbolsAtIndex) {
-        /*прохожу по массиву копируя значения с конца начиная с последнего элемента length - 1
+    //extend char[] - string and return string
+    public static String extendString(String string, char[] test) {
+        return string + new String(test);
+    }
+
+    //extend string - string and return string
+    public static String extendString(String str, String string) {
+        return str + string;
+    }
+
+    public static String addSymbolToString(String string, char symbol, int moveSymbolsAtIndex) {
+        //создаю массив char с длиной строки str увеличенной на 1
+        char[] arrayStr = new char[string.length() + 1];
+        //копирую символы строки str в массив char
+        copySymbolsToLargerArray(string, arrayStr);
+
+        /*прохожу по массиву descending с последнего элемента length - 1
         до индекса startMoveSymbolsAt
-        i - 1 = i
+        i = i - 1
          */
-        for (int i = arrChar.length - 1; i > moveSymbolsAtIndex; i--) {
-            arrChar[i] = arrChar[i - 1];
+        for (int i = arrayStr.length - 1; i > moveSymbolsAtIndex; i--) {
+            arrayStr[i] = arrayStr[i - 1];
         }
-        //в заданный индекс присваиваю символ add
-        arrChar[moveSymbolsAtIndex] = symbol;
+        //в заданный индекс присваиваю symbol
+        arrayStr[moveSymbolsAtIndex] = symbol;
+        return new String(arrayStr);
     }
 
-    public static void copySymbols(String str, char[] arrstr) {
-        for (int i = 0; i < str.length(); i++) {
-            arrstr[i] = str.charAt(i);
+    private static void copySymbolsToLargerArray(String string, char[] arrayStr) {
+        for (int i = 0; i < string.length(); i++) {
+            arrayStr[i] = string.charAt(i);
+        }
+    }
+
+    private static void copySymbolsToSmallerArray(String string, char[] arrayStr) {
+        for (int i = 0; i < arrayStr.length; i++) {
+            arrayStr[i] = string.charAt(i);
         }
     }
 }
