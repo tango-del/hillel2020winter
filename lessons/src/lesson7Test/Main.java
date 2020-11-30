b2840b47c163dff2f9210de425234717787ad5dd
tango <53480103+tango-del@users.noreply.github.com>
2020-11-30 15:40:55 +0200

    lesson7test

--- lessons/src/lesson7Test/Main.java
+++ lessons/src/lesson7Test/Main.java
@@ -4,21 +4,6 @@ import lesson7Test.Collection.StringList;
 
 public class Main {
     public static void main(String[] args) {
-//        List<String> list = new ArrayList<>();
-//
-//        String first = "First Person";
-//        String second = "Second Person";
-//
-//        list.add(first);
-//        list.add(second);
-//
-//        System.out.println(list);
-//
-//        boolean remove = list.remove(first);
-//
-//        System.out.println(remove);
-//        String person = list.get(0);
-//        System.out.println(person);
 
         StringList stringList = new StringList();
 
