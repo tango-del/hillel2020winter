d0c5d788f8e6bd2d3d18c423b623312f120e8b5e
Tango <53480103+tango-del@users.noreply.github.com>
2020-12-01 14:53:56 +0200

    lesson8 init

---
+++ lessons/src/lesson8/Main.java
@@ -0,0 +1,16 @@
+package lesson8;
+/*
+TODO Реализовать простую коллекцию для String на базе массива использую интерфейс
+ -Interface collection:
+ -boolean add(Object o);
+ -boolean add(index, Object o);
+ -boolean delete (Object o);
+ -Object get(int index);
+ -boolean contain(Object o);
+ -boolean equals (Collection str),
+ -boolean clear(),
+ -int size(),
+ */
+
+public class Main {
+}
