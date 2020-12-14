da615b56491d77ed84b9ac2be2b872351e17a718
Tango <53480103+tango-del@users.noreply.github.com>
2020-12-14 17:49:51 +0200

    lesson8 description

--- lessons/src/lesson8/Main.java
+++ lessons/src/lesson8/Main.java
@@ -18,7 +18,7 @@ import lesson8.Collection.StringList;
 public class Main {
     public static void main(String[] args) {
         //StringList
-        //workWithStringList();
+        workWithStringList();
 
         /*
        TODO Lesson 10 tasks:
@@ -29,10 +29,10 @@ public class Main {
 
         //HomeWork lesson 10
         //Iterator with remove duplicates
-        workWithIterator();
+        //workWithIterator();
 
         //корень квадратного уравнения
-        squareRoot();
+        //squareRoot();
     }
     public static void workWithStringList() {
         StringList string = new StringList();
@@ -42,33 +42,41 @@ public class Main {
         string.add("4");
         string.add("5");
         string.add("6");
-        System.out.println(string.add("7"));
-        string.add("8");
-        string.add("9");
-        string.add("10");
-        System.out.println(string.toString());
-        System.out.println(string.add("11", 10));
-        string.add("12", 11);
-        string.add("13");
-        System.out.println(string.toString());
-        string.remove(0);
-        System.out.println(string.remove(10));
-        System.out.println(string.remove("13"));
-        string.add("14");
-        string.add("15");
-        string.add("16");
-        string.remove("10");
-        string.add("17");
-        System.out.println(string.toString());
-        System.out.println(string.get(0));
-        System.out.println(string.get("0"));
-        System.out.println(string.size());
-        System.out.println(string.contains("3"));
-        System.out.println(string.contains("0"));
+        string.add("7", 5);
+        string.add("8", 5);
+        string.add("9", 5);
+        System.out.println(string.add("9", 13));
+//        string.add("8");
+//        string.add("9", 8);
 
-
-        System.out.println(string.clear());
-        System.out.println(string.toString());
+        System.out.println(string);
+//        System.out.println(string.add("7"));
+//        string.add("8");
+//        string.add("9");
+//        string.add("10");
+//        System.out.println(string.toString());
+//        System.out.println(string.add("11", 10));
+//        string.add("12", 11);
+//        string.add("13");
+//        System.out.println(string.toString());
+//        string.remove(0);
+//        System.out.println(string.remove(10));
+//        System.out.println(string.remove("13"));
+//        string.add("14");
+//        string.add("15");
+//        string.add("16");
+//        string.remove("10");
+//        string.add("17");
+//        System.out.println(string.toString());
+//        System.out.println(string.get(0));
+//        System.out.println(string.get("0"));
+//        System.out.println(string.size());
+//        System.out.println(string.contains("3"));
+//        System.out.println(string.contains("0"));
+//
+//
+//        System.out.println(string.clear());
+//        System.out.println(string.toString());
     }
 
     public static void workWithIterator() {
