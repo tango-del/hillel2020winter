be37b28d09c1fb59652acffd52de95b83390b777
Tango <53480103+tango-del@users.noreply.github.com>
2021-01-06 13:45:43 +0200

    lesson17 practise FileInputStream FileOutputStream

--- lessons/src/lesson17practise/io/byteStream/ByteStream.java
+++ lessons/src/lesson17practise/io/byteStream/ByteStream.java
@@ -1,5 +1,30 @@
 package lesson17practise.io.byteStream;
 
-public class ByteStream {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
+public class ByteStream {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fis = null;
+        FileOutputStream fos = null;
+        try {
+            //fis = new FileInputStream("C:\\Users\\Tango\\IdeaProjects\\hillel2020winter\\lessons\\src\\lesson17practise\\io\\byteStream\\input.txt"); // -> from
+            //fos = new FileOutputStream("C:\\Users\\Tango\\IdeaProjects\\hillel2020winter\\lessons\\src\\lesson17practise\\io\\byteStream\\output.txt"); // -> to
+            fis = new FileInputStream("C:\\Users\\Tango\\IdeaProjects\\hillel2020winter\\lessons\\src\\lesson17practise\\io\\byteStream\\logo.jpg");
+            fos = new FileOutputStream("C:\\Users\\Tango\\IdeaProjects\\hillel2020winter\\lessons\\src\\lesson17practise\\io\\byteStream\\resultlogo.jpg");
+            int a;
+            while ((a = fis.read()) != -1) {
+                fos.write(a);
+            }
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (fis != null) fis.close();
+            if (fos != null) fos.close();
+        }
+    }
 }
