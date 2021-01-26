712e6e09993a168234198e2705f4e8d8266ef1ae
Tango <53480103+tango-del@users.noreply.github.com>
2021-01-26 17:13:32 +0200

    move maven projects to lessons package

--- lessons/src/restExample/src/main/java/hello/StudentController.java
+++ lessons/src/restExample/src/main/java/hello/StudentController.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.UUID;
 import java.util.stream.Collectors;
 
