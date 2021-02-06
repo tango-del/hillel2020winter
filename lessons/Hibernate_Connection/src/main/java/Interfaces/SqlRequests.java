b141ef26bf65f797c01467f020c62b7b7c59d251
Tango <53480103+tango-del@users.noreply.github.com>
2021-02-06 02:32:40 +0200

    Hibernate Connection delete unused import

--- lessons/Hibernate_Connection/src/main/java/Interfaces/SqlRequests.java
+++ lessons/Hibernate_Connection/src/main/java/Interfaces/SqlRequests.java
@@ -3,8 +3,6 @@ package Interfaces;
 import entities.Student;
 import org.hibernate.Session;
 
-import java.sql.SQLException;
-
 public interface SqlRequests {
     void addNewEntityStudent(Session session , Student student);
     void deleteEntityStudentByID(Session session, int students_id);
