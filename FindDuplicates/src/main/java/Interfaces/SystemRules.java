c3d4f97f4111feb92f826feca8963f27de89e096
Tango <53480103+tango-del@users.noreply.github.com>
2021-02-07 15:25:40 +0200

    FindDuplicate delete and create new repo

--- FindDuplicates/src/main/java/Interfaces/SystemRules.java
+++
@@ -1,8 +0,0 @@
-package Interfaces;
-
-import java.io.IOException;
-
-public interface SystemRules {
-    boolean systemExclusionRules(String OS) throws IOException;
-    void writePropValuesToStrings() throws IOException;
-}
